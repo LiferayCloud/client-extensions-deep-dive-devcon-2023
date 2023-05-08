2eb3acd08695f9c976545a0022cb0f231f7935fc
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-05-08 18:18:22 -0500

    Get custom element working

--- client-extensions/current-tickets-custom-element/src/App.jsx
+++ client-extensions/current-tickets-custom-element/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edits <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
--- client-extensions/current-tickets-custom-element/vite.config.js
+++ client-extensions/current-tickets-custom-element/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+	origin: 'http://localhost:5173'
+ }
 })
