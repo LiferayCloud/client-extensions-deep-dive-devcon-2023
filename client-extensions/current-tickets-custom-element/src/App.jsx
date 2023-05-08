b936cd31c5312164209fb54291c9e61cd43e478b
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-05-08 18:20:31 -0500

    Add react-data-grid

--- client-extensions/current-tickets-custom-element/src/App.jsx
+++ client-extensions/current-tickets-custom-element/src/App.jsx
@@ -2,33 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'react-data-grid/lib/styles.css';
+import DataGrid from 'react-data-grid';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [rows, setRows] = useState([]);
+
+  const columns = [
+	{ key: 'id', name: 'ID' },
+	{ key: 'title', name: 'Title' }
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edits <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <DataGrid columns={columns} rows={rows} onRowsChange={setRows} />
   )
 }
 
