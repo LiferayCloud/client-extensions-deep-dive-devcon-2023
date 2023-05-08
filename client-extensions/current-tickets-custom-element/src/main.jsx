b9df96438db0fa4a11130cab71181f7abb737d08
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-05-08 18:18:22 -0500

    Get custom element working

--- client-extensions/current-tickets-custom-element/src/main.jsx
+++ client-extensions/current-tickets-custom-element/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+class WebComponent extends HTMLElement {
+	connectedCallback() {
+		// eslint-disable-next-line @liferay/portal/no-react-dom-render
+		ReactDOM.render(
+			<React.StrictMode>
+				<App route={this.getAttribute('route') || '/'} />
+			</React.StrictMode>,
+			this
+		);
+	}
+}
+const ELEMENT_ID = 'current-tickets-custom-element';
+
+if (!customElements.get(ELEMENT_ID)) {
+	customElements.define(ELEMENT_ID, WebComponent);
+}
