b6a8774e0d00f078ebb51588ec0e0276212bd27c
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-05-08 23:29:05 -0500

    Fix build issues

--- client-extensions/current-tickets-custom-element/vite.config.js
+++ client-extensions/current-tickets-custom-element/vite.config.js
@@ -5,6 +5,9 @@ import {defineConfig} from 'vite';
 // https://vitejs.dev/config/
 
 export default defineConfig({
+	build: {
+		outDir: 'build',
+	},
 	plugins: [react()],
 	server: {
 		origin: 'http://localhost:5173',
