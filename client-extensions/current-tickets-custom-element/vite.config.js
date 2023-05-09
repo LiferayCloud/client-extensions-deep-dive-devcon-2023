fd40ce79f7c4f38c003941cae3668cbbbe5a87b5
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-05-08 19:47:08 -0500

    SF

--- client-extensions/current-tickets-custom-element/vite.config.js
+++ client-extensions/current-tickets-custom-element/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-	origin: 'http://localhost:5173'
- }
-})
+	plugins: [react()],
+	server: {
+		origin: 'http://localhost:5173',
+	},
+});
