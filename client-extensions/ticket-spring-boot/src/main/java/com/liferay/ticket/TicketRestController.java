44523588aad0fa5e1f61e65176cd99d4e233a4a5
Raymond Augé <raymond.auge@liferay.com>
2023-05-16 10:01:36 -0400

    [ticket-spring-boot] update ticket based on content - initial

--- client-extensions/ticket-spring-boot/src/main/java/com/liferay/ticket/TicketRestController.java
+++ client-extensions/ticket-spring-boot/src/main/java/com/liferay/ticket/TicketRestController.java
@@ -14,13 +14,17 @@
 
 package com.liferay.ticket;
 
+import java.util.Objects;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -28,6 +32,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import reactor.core.publisher.Mono;
 
 /**
  * @author Raymond Augé
@@ -52,9 +60,7 @@ public class TicketRestController {
 			_log.info("JWT Subject: " + jwt.getSubject());
 
 			try {
-				JSONObject jsonObject = new JSONObject(json);
-
-				_log.info("\n\n" + jsonObject.toString(4) + "\n");
+				_maybeQueueTicket(jwt.getTokenValue(), json);
 			}
 			catch (Exception exception) {
 				_log.error("JSON: " + json, exception);
@@ -62,6 +68,73 @@ public class TicketRestController {
 		}
 
 		return new ResponseEntity<>(json, HttpStatus.CREATED);
+	}
+
+	private void _maybeQueueTicket(String jwtToken, String json) {
+		Objects.requireNonNull(json);
+
+		JSONObject jsonObject = new JSONObject(json);
+
+		_log.info("JSON INPUT: \n\n" + jsonObject.toString(4) + "\n");
+
+		JSONObject jsonTicketDTO = jsonObject.getJSONObject("objectEntryDTOTicket");
+		JSONObject jsonProperties = jsonTicketDTO.getJSONObject("properties");
+		JSONObject jsonTicketStatus = jsonProperties.getJSONObject("ticketStatus");
+
+		if (!Objects.equals(jsonTicketStatus.getString("key"), "open")) {
+			_log.info("Ticket Not OPEN. Nothing to do!");
+
+			return;
+		}
+
+		jsonTicketStatus.remove("name");
+		jsonTicketStatus.put("key", "queued");
+		jsonProperties.put(
+			"description",
+			jsonProperties.getString("description") + "<br/>MORE DEAILS!!!");
+
+		_log.info("JSON OUTPUT: \n\n" + jsonProperties.toString(4) + "\n");
+
+		WebClient.Builder builder = WebClient.builder();
+
+		WebClient webClient = builder.baseUrl(
+			_lxcDXPServerProtocol + "://" + _lxcDXPMainDomain
+		).defaultHeader(
+			HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE
+		).defaultHeader(
+			HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE
+		).build();
+
+		webClient.patch(
+		).uri(
+			"/o/c/tickets/{ticketId}", jsonTicketDTO.getLong("id")
+		).bodyValue(
+			jsonProperties.toString()
+		).header(
+			HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken
+		).exchangeToMono(
+			clientResponse -> {
+				HttpStatus httpStatus = clientResponse.statusCode();
+
+				if (httpStatus.is2xxSuccessful()) {
+					return clientResponse.bodyToMono(String.class);
+				}
+				else if (httpStatus.is4xxClientError()) {
+					return Mono.just(httpStatus.getReasonPhrase());
+				}
+
+				Mono<WebClientResponseException> mono =
+					clientResponse.createException();
+
+				return mono.flatMap(Mono::error);
+			}
+		).doOnNext(
+			output -> {
+				if (_log.isInfoEnabled()) {
+					_log.info("Output: " + output);
+				}
+			}
+		).subscribe();
 	}
 
 	private static final Log _log = LogFactory.getLog(
